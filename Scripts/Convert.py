99398591884207d0d0c75cc18a27adf165ee37bd
Julian Hermes <julian@hermesfamilie.de>
2024-04-24 13:46:57 +0200

    more functionallity and better file naming but buggy output

--- Scripts/Convert.py
+++ Scripts/Convert.py
@@ -1,20 +1,29 @@
+
 import re
 import random
 import PyPDF2
 
+counterFile = open("C:\Projects\ConvertPDF\\numberPDF.txt","r")
+numberPDFs = int(counterFile.read())
+counterFile.close()
+counterFile = open("C:\Projects\ConvertPDF\\numberPDF.txt","w")
+counterFile.write(str(numberPDFs + 1))
+counterFile.close()
 
-pdf = open('C:\Projects\ConvertPDF\pdf1.pdf','rb')
+
+pdfName = "pdf1"
+pdf = open("C:\Projects\ConvertPDF\\" + pdfName + ".pdf","rb")
 pdfreader = PyPDF2.PdfReader(pdf)
 x = len(pdfreader.pages)
 pdf = pdfreader.pages[0]
 text = pdf.extract_text()
 
-#randomise the path of txt to not delete old ones
-rand = random.randint(0,100000)
-textDocPath = "C:\Projects\ConvertPDF\\TextOutput" + str(rand) + ".txt"
+
+textDocPath = "C:\Projects\ConvertPDF\\TextOutput" + str(numberPDFs) + "_From_" + pdfName + ".txt"
 
 file1 = open(textDocPath,"a")
 file1.writelines(text)
 file1.close()
+
 
 
